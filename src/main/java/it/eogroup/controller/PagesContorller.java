35b3f5ffb66353a0956c5d09b3dfd2b6bdf694d5
Letu <1198350444@qq.com>
2021-02-03 20:43:04 +0800

    个人页面-前端1.1

--- src/main/java/it/eogroup/controller/PagesContorller.java
+++ src/main/java/it/eogroup/controller/PagesContorller.java
@@ -53,4 +53,9 @@ public class PagesContorller {
         return "register";
     }
 
+    @RequestMapping("/accountInfo")
+    public String toAccountInfo() {
+        return "userInfo";
+    }
+
 }
